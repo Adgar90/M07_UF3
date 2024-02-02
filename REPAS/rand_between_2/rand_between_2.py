95a127d1435cd76ae3e08af2f6d8fd129ec90563
unknown <adriagp90@gmail.com>
2024-02-02 17:10:48 +0100

    fet 28

---
+++ REPAS/rand_between_2/rand_between_2.py
@@ -0,0 +1,4 @@
+from random import randrange
+#Funció que donats dos nombres, et retorna un aleatori entre aquest dos números
+def aleatori(x, y):
+    return int(randrange(x, y))
--- REPAS/suma_nums/suma_nums.py
+++ REPAS/suma_nums/suma_nums.py
@@ -1,3 +1,4 @@
+#Funció que retorna la suma de dos nombres passats per paràmetre
 def suma(x, y):
 
     return x+y
